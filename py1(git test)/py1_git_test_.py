863612c35c34e4156b997a4985d6b68100b162d5
lv <781262125@qq.com>
2018-06-29 09:57:28 +0800

    命名关键字参数

--- py1(git test)/py1_git_test_.py
+++ py1(git test)/py1_git_test_.py
@@ -1,4 +1,14 @@
-a = 1
-b = 2
-print(a+b)
-print(a*b)
+# -*- coding: utf-8 -*-   
+
+list = ['abc','efg','anc']
+
+for str1 in list:
+    print(str1)
+
+def f2(a, b, c=0, *, d, **kw):
+    print('a =', a, 'b =', b, 'c =', c, 'd =', d, 'kw =', kw)
+
+args = (1, 2)
+kw = {'d': 99, 'x': '#'}
+
+f2(*args, **kw)
